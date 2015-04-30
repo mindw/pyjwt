9433351502c84df27462d096d5afa41bd7c6d178
Gabi Davar <grizzly.nyo@gmail.com>
2015-04-30 19:18:58 +0300

    remove jwt script

--- setup.py
+++ setup.py
@@ -53,10 +53,5 @@ setup(
             'flake8-import-order',
             'pep8-naming'
         ]
-    ),
-    entry_points={
-        'console_scripts': [
-            'jwt = jwt.__main__:main'
-        ]
-    }
+    )
 )
